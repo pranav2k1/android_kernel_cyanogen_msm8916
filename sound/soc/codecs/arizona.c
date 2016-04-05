CirrusLogic/linux-drivers

ASoC: moon: don't change osr when input is in analog mode
In analog mode osr is fixed to the power on reset value of
3.072MHz and setting it to any lower value significantly
detoriates the audio quality so this patch blocks any
changes of osr when input is in analog mode

Change-Id: I874e021d8b04ad15a29cfdc972561d2f5ce8932b
Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

sound/soc/codecs/arizona.c
@@ -2250,6 +2250,25 @@ exit:
 }
 EXPORT_SYMBOL_GPL(moon_dfc_put);
 
+int moon_osr_put(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
+	unsigned int mode;
+
+	/* for analog mode osr is fixed */
+	mode = snd_soc_read(codec, e->reg - 2);
+	if (!(mode & ARIZONA_IN1_MODE_MASK)) {
+		dev_err(codec->dev,
+			"OSR is fixed to 3.072MHz in analog mode\n");
+		return -EINVAL;
+	}
+
+	return snd_soc_put_enum_double(kcontrol, ucontrol);
+}
+EXPORT_SYMBOL_GPL(moon_osr_put);
+
 static void arizona_in_set_vu(struct snd_soc_codec *codec, int ena)
 {
 	struct arizona_priv *priv = snd_soc_codec_get_drvdata(codec);

sound/soc/codecs/moon.c
@@ -675,8 +675,10 @@ static DECLARE_TLV_DB_SCALE(ng_tlv, -10200, 600, 0);
 	{ name " ANC Source", "RXANCR", "RXANCR" }
 
 static const struct snd_kcontrol_new moon_snd_controls[] = {
-SOC_ENUM("IN1 OSR", clearwater_in_dmic_osr[0]),
-SOC_ENUM("IN2 OSR", clearwater_in_dmic_osr[1]),
+SOC_ENUM_EXT("IN1 OSR", clearwater_in_dmic_osr[0],
+	snd_soc_get_enum_double, moon_osr_put),
+SOC_ENUM_EXT("IN2 OSR", clearwater_in_dmic_osr[1],
+	snd_soc_get_enum_double, moon_osr_put),
 SOC_ENUM("IN3 OSR", clearwater_in_dmic_osr[2]),
 SOC_ENUM("IN4 OSR", clearwater_in_dmic_osr[3]),
 SOC_ENUM("IN5 OSR", clearwater_in_dmic_osr[4]),

pranav2k1/android_kernel_cyanogen_msm8916

Change-Id: Id4d74098cd7a879c9935a81952c1123ac8b3bf5d
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

sound/soc/codecs/arizona.c
@@ -2303,6 +2303,25 @@ exit:
 }
 EXPORT_SYMBOL_GPL(moon_dfc_put);
 
+int moon_osr_put(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_kcontrol_chip(kcontrol);
+	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
+	unsigned int mode;
+
+	/* for analog mode osr is fixed */
+	mode = snd_soc_read(codec, e->reg - 2);
+	if (!(mode & ARIZONA_IN1_MODE_MASK)) {
+		dev_err(codec->dev,
+			"OSR is fixed to 3.072MHz in analog mode\n");
+		return -EINVAL;
+	}
+
+	return snd_soc_put_value_enum_double(kcontrol, ucontrol);
+}
+EXPORT_SYMBOL_GPL(moon_osr_put);
+
 static void arizona_in_set_vu(struct snd_soc_codec *codec, int ena)
 {
 	struct arizona_priv *priv = snd_soc_codec_get_drvdata(codec);

sound/soc/codecs/moon.c
@@ -675,8 +675,10 @@ static DECLARE_TLV_DB_SCALE(ng_tlv, -10200, 600, 0);
 	{ name " ANC Source", "RXANCR", "RXANCR" }
 
 static const struct snd_kcontrol_new moon_snd_controls[] = {
-SOC_VALUE_ENUM("IN1 OSR", clearwater_in_dmic_osr[0]),
-SOC_VALUE_ENUM("IN2 OSR", clearwater_in_dmic_osr[1]),
+SOC_VALUE_ENUM_EXT("IN1 OSR", clearwater_in_dmic_osr[0],
+	snd_soc_get_value_enum_double, moon_osr_put),
+SOC_VALUE_ENUM_EXT("IN2 OSR", clearwater_in_dmic_osr[1],
+	snd_soc_get_value_enum_double, moon_osr_put),
 SOC_VALUE_ENUM("IN3 OSR", clearwater_in_dmic_osr[2]),
 SOC_VALUE_ENUM("IN4 OSR", clearwater_in_dmic_osr[3]),
 SOC_VALUE_ENUM("IN5 OSR", clearwater_in_dmic_osr[4]),